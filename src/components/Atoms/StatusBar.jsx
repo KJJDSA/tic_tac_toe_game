06b718211f7b4cca9e81a5108901d3a5b2f19686
KJJDSA <kjjdsa@gmail.com>
2025-04-22 19:01:55 +0900

    Atomic 디자인 패턴에 따라 컴포넌트 구조 짜기 2

---
+++ src/components/Atoms/StatusBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const StatusBar = ({target, status}) => {
+  const data = {
+    targetData: ["플레이어1", "플레이어2"],
+    statusData: {
+      turn: "턴",
+      win: "승리",
+      lose: "패배",
+    }
+  }
+  return (
+    <div>
+      <p>{data.targetData[target]} 의 {data.statusData[status]}</p>
+    </div>
+  )
+}
+
+export default StatusBar
--- src/components/Molecules/TicTacToe.jsx
+++ src/components/Molecules/TicTacToe.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import StatusBar from '../Atoms/StatusBar'
 
 export default class TicTacToe extends Component {
   render() {
     return (
       <div>
         <img src="src/assets/tictactoeSample.png" alt="" />
-        <p>status: player 1 승리!</p>
+        <StatusBar target={0} status="win" />
       </div>
     )
   }
